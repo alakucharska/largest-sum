2b9e911f36bcd54234adc2d5406a0c65e05c57c5
alakucharska <ala.kucharska@gmail.com>
2022-10-15 21:07:07 +0200

    Include that digit 9 cannot be changed

--- largest_sum.rb
+++ largest_sum.rb
@@ -1,54 +1,77 @@
 class LargestSum
 
-  def initialize(num)
-    @num = num
+  def initialize(array_of_numbers)
+    @array_of_numbers = array_of_numbers
   end
 
-  attr_reader :num
+  attr_reader :array_of_numbers
 
   def biggest_number_after_changes
-    arr1 = []
+    return array_of_numbers if all_digits_9?
+
+    final_result = []
     if number_of_remaining_changes == 3 || number_of_remaining_changes == 6
-      hash = { numbers_divisible_by_3.max => number_of_remaining_changes }
+      hash = { biggest_number_possible => number_of_remaining_changes } 
       hash.each do |biggest_number, remaining_changes|
-        arr1 << add_number_of_changes_to_the_number(biggest_number, remaining_changes)
+        final_result << add_number_of_changes_to_the_number(biggest_number, remaining_changes)
       end
-      i = numbers_divisible_by_3.index(numbers_divisible_by_3.max)
-      numbers_divisible_by_3[i] = arr1[0]
-      p numbers_divisible_by_3
+      index_of_the_number_to_be_replaced = numbers_divisible_by_3.index(biggest_number_possible)
+      numbers_divisible_by_3[index_of_the_number_to_be_replaced] = final_result[0]
+      numbers_divisible_by_3
     else
-      p numbers_divisible_by_3
+      numbers_divisible_by_3 
     end
   end
 
   private
+
+  def all_digits_9?
+    array_of_numbers.map do |number|
+      return false if number.abs.digits.any? { |value| value != 9 }
+    end
+  end
     
   def changes_needed_to_get_numbers_divisible_by_3
-    result = num.map{|n| n%3} 
-    result.map do |dif| 
-      dif != 0 ? 3-dif : 0
+    modulo_result = array_of_numbers.map{ |number| number%3 } 
+    modulo_result.map do |value| 
+      value != 0 ? 3 - value : 0  
     end
   end
 
   def assign_changes_to_numbers
-    Hash[num.zip(changes_needed_to_get_numbers_divisible_by_3)] 
+    Hash[ array_of_numbers.zip(changes_needed_to_get_numbers_divisible_by_3) ] 
   end
 
-  def add_number_of_changes_to_the_number(number, changes)
-    power = number.abs.digits.count
+  def position_of_digit_to_change(number) 
+    position = 0
+    number.abs.digits.reverse.map do |digit|
+      break if digit != 9 || number.digits.all? {|value| value == 9}
+      position += 1 
+    end
+    position
+  end
+
+  def add_number_of_changes_to_the_number(number, changes) 
+    power = number.abs.digits.count - position_of_digit_to_change(number)
     number + (changes*(10**(power-1)))
   end
 
   def change_numbers_to_be_the_biggest_divisible_by_3
-    arr = []
-    assign_changes_to_numbers.each do |number, changes| 
-      arr << add_number_of_changes_to_the_number(number, changes)
-    end
-      arr
+    array_of_numbers_divisible_by_3 = []
+      assign_changes_to_numbers.each do |number, changes| 
+        array_of_numbers_divisible_by_3 << add_number_of_changes_to_the_number(number, changes)
+      end
+    array_of_numbers_divisible_by_3              
   end
 
   def numbers_divisible_by_3
-    @numbers ||= change_numbers_to_be_the_biggest_divisible_by_3
+    @divisors_of_3 ||= change_numbers_to_be_the_biggest_divisible_by_3
+  end
+
+  def biggest_number_possible
+    numbers_divisible_by_3.sort.reverse.find do |element| 
+      !element.abs.digits.all? { |value| value == 9 }
+    end
   end
 
   def number_of_remaining_changes
@@ -65,6 +88,6 @@ class LargestSum
 end
 
 puts "Please enter 3 numbers"
-num = gets.chomp.split(/[\s.,]+/).map(&:to_i)
+array_of_numbers = gets.chomp.split(/[\s.,]+/).map(&:to_i)
 
-LargestSum.new(num).biggest_number_after_changes  
+LargestSum.new(array_of_numbers).biggest_number_after_changes  
--- spec/largest_sum_spec.rb
+++ spec/largest_sum_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe LargestSum do
     expect(LargestSum.new(num).biggest_number_after_changes).to eq([3,3,9])
   end
 
-  it "adds 3 to the firts digit in a number if number of changes is >=3 and <6" do
+  it "adds 3 to the respective digit in a number if number of changes is >=3 and <6" do
     num = [12,14,17]
 
     expect(LargestSum.new(num).biggest_number_after_changes).to eq([12,24,57])
@@ -20,10 +20,10 @@ RSpec.describe LargestSum do
     expect(LargestSum.new(num).biggest_number_after_changes).to eq([30,24,6])
   end
 
-  xit "returns the same number of elements as in the original array" do
-    num = [3,3,6]
+  it "adds 6 to the respective digit in a number if number of changes == 6" do
+    num = [3,6,12]
 
-    expect(LargestSum.new(num).biggest_number_after_changes).to eq([3,3,12])
+    expect(LargestSum.new(num).biggest_number_after_changes).to eq([3,6,72])
   end
 
   it "accepts negative numbers" do
@@ -35,6 +35,24 @@ RSpec.describe LargestSum do
   it "accepts negative and positive numbers in one array" do
     num = [-2006, 5, 99]
 
-    expect(LargestSum.new(num).biggest_number_after_changes).to eq([-6,6,129])
+    expect(LargestSum.new(num).biggest_number_after_changes).to eq([-6,9,99])
+  end
+
+  it "does not make any changes to 9" do
+    num = [4, 7, 9]
+
+    expect(LargestSum.new(num).biggest_number_after_changes).to eq([6, 9, 9])
+  end
+
+  it "returns the original array of numbers if all numbers consist only of digit 9" do
+    num = [99, 99, 99]
+
+    expect(LargestSum.new(num).biggest_number_after_changes).to eq([99, 99, 99])
+  end
+
+  xit "returns the same number of elements as in the original array" do
+    num = [3,3,6]
+
+    expect(LargestSum.new(num).biggest_number_after_changes).to eq([3,3,12])
   end
 end
